fcffe6e267c81763c80c33e98b0997be5a0ed25f
Michael Egbinola <michaelmarketing25@gmail.com>
2022-09-15 09:00:57 -0700

    installed nodemon in the 2.Nodejs folder

--- 2.Nodejs/app.js
+++ 2.Nodejs/app.js
@@ -1,11 +1,48 @@
+const fs = require("fs"); //fileSystem Module
+const path = require("path"); //Path Module
+
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", function (req, res) {
-  res.send("<h1>Understanding Nodejs with Express</h1>"); //localhost:3000/
+  //   res.send("<h1>Understanding Nodejs with Express</h1>"); //localhost:3000/
+  res.send(
+    "<form action='/store-user' method='POST'><label for='username'>UserNAME </label><input type='text' id='username' name='username'><hr><button type='reset'>Reset</button<hr><button type='submit'>Submit</button></form>"
+  );
 });
 
 app.get("/currenttime", function (req, res) {
   res.send("<h1>" + new Date().toISOString() + "</h1>"); //localhost:3000/currenttime
 });
+
+app.post("/store-user", function (req, res) {
+  const userName = req.body.username;
+
+  const filePath = path.join(__dirname, "data", "users.json"); //access the absolute file path
+  const fileData = fs.readFileSync(filePath); //reads the file data
+  const existingUsers = JSON.parse(fileData); //parse the file data to js code
+  existingUsers.push(userName);
+
+  fs.writeFileSync(filePath, JSON.stringify(existingUsers)); //convert js code to json
+
+  console.log(userName);
+  res.send("<h1>Form Submitted Successfully!</h1>");
+});
+
+app.get("/users", function (req, res) {
+  const filePath = path.join(__dirname, "data", "users.json"); //access the absolute file path
+  const fileData = fs.readFileSync(filePath); //reads the file data
+  const existingUsers = JSON.parse(fileData); //parse the file data to js code
+
+  let responseData = "<ul>";
+  for (const user of existingUsers) {
+    responseData += "<li>" + user + "</li>";
+  }
+  responseData += "</ul>";
+
+  res.send(responseData);
+});
+
 app.listen(3000);
